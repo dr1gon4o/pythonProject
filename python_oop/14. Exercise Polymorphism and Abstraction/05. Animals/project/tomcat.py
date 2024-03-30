ae0733f6996bdf9e2a698186b20c47b6e7a0d503
dr1gon4o <146662391+dr1gon4o@users.noreply.github.com>
2024-03-31 00:31:54 +0200

    Initial commit

---
+++ python_oop/14. Exercise Polymorphism and Abstraction/05. Animals/project/tomcat.py
@@ -0,0 +1,11 @@
+from project.cat import Cat
+
+
+class Tomcat(Cat):
+
+    def __init__(self, name, age):
+        self.gender = "Male"
+        super().__init__(name, age, self.gender)
+
+    def make_sound(self):
+        return f"Hiss"
